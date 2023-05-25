fcf2ebaa4ad34e1199a6777e530520b69cdbdd42
nicole-beatrice-go <119998974+nicole-beatrice-go@users.noreply.github.com>
2023-05-25 09:29:48 -0700

    added bash script

--- grade.sh
+++ grade.sh
@@ -8,6 +8,29 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+set -e
+
+if [[ -f "student-submission/ListExamples.java" ]] 
+then
+    echo "Correct file submitted!"
+else 
+    echo "Wrong file submitted!"
+fi
+
+cp -r *.java grading-area
+cp -r lib grading-area
+
+set +e
+
+javac student-submission/ListExamples.java
+
+if [[ $? != 0 ]] 
+then
+    echo "Oh no! Program couldn't compile!"
+else 
+    echo "Yay! Program compiled successfully!"
+fi
+
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
